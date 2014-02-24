812cdba178ade07991f27e86dd058039a0141d1d
Your Name <your.email@example.com>
2014-02-23 20:31:59 -0600

    improve

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Second Project'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Second Project')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("This is our Second Project| Home")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("This is our Second Project| About Us")
+    end
+  end
+  
+  describe "Blog page" do
+
+    it "should have the content 'Blog'" do
+      visit '/static_pages/blog'
+      expect(page).to have_content('Blog')
+    end
+    it "should have the right title" do
+      visit '/static_pages/blog'
+      expect(page).to have_title("This is our Second Project| Blog")
+    end
+    
+  end
+  describe "FAQ page" do
+
+    it "should have the content 'FAQ'" do
+      visit '/static_pages/faq'
+      expect(page).to have_content('FAQ')
+    end
+    it "should have the right title" do
+      visit '/static_pages/faq'
+      expect(page).to have_title("This is our Second Project| FAQ")
+    end
+  end
+  
+  
+end
